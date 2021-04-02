2cc9de8e2752dd30fef46c7e0dfd3c1761c363a4
undefined <sjx233@qq.com>
2021-04-02 18:07:04 +0800

    Stop rollup from complaining

--- packages/renderer/vite.config.ts
+++ packages/renderer/vite.config.ts
@@ -7,11 +7,28 @@ function preprocessor(plugin: Plugin): Plugin {
   return { ...plugin, enforce: "pre" };
 }
 
+function fixReactVirtualized(): Plugin {
+  return {
+    name: "fix-react-virtualized",
+    enforce: "pre",
+    transform(code, id) {
+      if (id.endsWith("/node_modules/react-virtualized/dist/es/WindowScroller/utils/onScroll.js")) {
+        return {
+          code: code.replace("import { bpfrpt_proptype_WindowScroller } from \"../WindowScroller.js\";", ""),
+          map: null
+        };
+      }
+      return null;
+    }
+  };
+}
+
 export default defineConfig(({ command }) => ({
   root: "./packages/renderer",
   base: "",
   plugins: [
     command === "build" ? preprocessor(eslint({ include: ["./packages/renderer/src/**/*.ts", "./packages/renderer/src/**/*.tsx"] })) : [],
+    fixReactVirtualized(),
     reactRefresh(),
     minifyHtml()
   ],
